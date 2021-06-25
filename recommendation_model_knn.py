d05f34e0952ad97d213aa7677f3e41c79892f60b
fra-mari <formicamarinaresca@gmail.com>
2021-06-25 15:33:57 +0200

    slight reshaping

--- recommendation_model_knn.py
+++ recommendation_model_knn.py
@@ -8,10 +8,11 @@ import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
+import pickle
 # %%
 # loading the data
-ratings = pd.read_csv('data/ratings.csv')
-movies = pd.read_csv('data/movies.csv')
+ratings = pd.read_csv('data/raw/ratings.csv')
+movies = pd.read_csv('data/raw/movies.csv')
 
 # %%
 final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
@@ -30,17 +31,19 @@ knn = NearestNeighbors(metric='cosine',
                         algorithm='brute',
                         n_neighbors=20,
                         n_jobs=-1)
-knn.fit(csr_data)
+model = knn.fit(csr_data)
 
-# %%
+with open('webapp/models/nmf.pickle', 'wb') as f:
+    # Pickle the model
+    pickle.dump(model, f)
 
-def get_movie_recommendation(movie_name):
+def get_recommendations(movie_name):
     n_movies_to_reccomend = 10
     movie_list = movies[movies['title'].str.contains(movie_name)]  
     if len(movie_list):        
         movie_idx= movie_list.iloc[0]['movieId']
         movie_idx = final_dataset[final_dataset['movieId'] == movie_idx].index[0]
-        distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
+        distances , indices = model.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
         rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
         recommend_frame = []
         for val in rec_movie_indices:
@@ -55,4 +58,9 @@ def get_movie_recommendation(movie_name):
 get_movie_recommendation('Iron Man')
 # %%
 get_movie_recommendation('Beautiful Mind')
+
+
+# %%
+
+
 # %%
